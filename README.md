496726ac9780ec4bcb7c6556c2a7eb2c1f5304b9
Alex Shpak <alex-shpak@users.noreply.github.com>
2026-01-06 00:50:41 +0100

    Improve CLI and readme file

--- README.md
+++ README.md
@@ -1,30 +1,71 @@
-# Yatch (YAML patch)
-A CLI and a library to patch YAML files while preserving formatting.
+# yatch
 
-# Usage
-## Usage as CLI
-```
+A tool for patching YAML files while preserving formatting and comments.
+
+## Installation
+
+```bash
 go install github.com/alex-shpak/yatch/cmd/yatch@latest
-yatch [-in=file] [-out=file] '.spec.image.tag' 'qwerty'
 ```
 
-## Usage as library
-```shell
+## CLI Usage
+
+```bash
+# Update value from stdin to stdout
+yatch '$.image.tag' 'v1.2.3' < input.yaml > output.yaml
+
+# Update files in-place
+yatch -in values.yaml -out values.yaml '$.version' '2.0'
+
+# Update a comment instead of the value
+yatch -in values.yaml --comment '$.service.port' 'Exposed port'
+
+# Pipe from other commands
+cat deployment.yaml | yatch '$.spec.replicas' '3'
+```
+
+### Options
+
+- `-in` - Input YAML file (default: stdin)
+- `-out` - Output YAML file (default: stdout)
+- `--comment` - Update comment attached to the node instead of the node value
+
+## Library Usage
+
+```bash
 go get github.com/alex-shpak/yatch/lib/yatch
 ```
 
 ```go
-/* Read file or stdin above */
-file, err := yatch.NewFile(reader)
-if err != nil {
-  return err
-}
+package main
 
-err := file.Patch(".spec.image.tag", "0.0.1")
-if err != nil {
-  return err
-}
+import (
+    "os"
+    "github.com/alex-shpak/yatch/lib/yatch"
+)
 
-yamlfile.Content()
+func main() {
+    file, err := yatch.NewFile(reader)
+    if err != nil {
+        panic(err)
+    }
+
+    // Patch a value
+    err = file.Patch("$.spec.image.tag", "v1.2.3")
+    if err != nil {
+        panic(err)
+    }
+
+    // Patch a comment
+    err = file.PatchComment("$.spec.image.tag", "{\"$imagepolicy\": \"flux-system:my-policy\"}")
+    if err != nil {
+        panic(err)
+    }
+
+    // Get updated content
+    updated := file.Content()
+}
 ```
 
+## How It Works
+`yatch` parses YAML using `goccy/go-yaml` library to an AST, locates the target node using path expression, and performs byte-level replacement to preserve the original formatting, indentation, comments.
--- cmd/yatch/main.go
+++ cmd/yatch/main.go
@@ -10,77 +10,92 @@ import (
 	"github.com/alex-shpak/yatch/lib/yatch"
 )
 
-var (
-	in, out *string
-	comment *bool
-)
-
 func main() {
-	in = flag.String("in", "", "input file")
-	out = flag.String("out", "", "output file")
-	comment = flag.Bool("comment", false, "update a comment attached to the node")
+	var (
+		in      = flag.String("in", "", "input YAML file (default: stdin)")
+		out     = flag.String("out", "", "output YAML file (default: stdout)")
+		comment = flag.Bool("comment", false, "update a comment instead of the node value")
+	)
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Usage: %s [options] <jsonpath> <value>
+
+Patch YAML files using JSONPath expressions.
+
+Options:
+`, os.Args[0])
+		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, `
+Examples:
+  # Update from stdin to stdout
+  %[1]s '$.image.tag' 'v1.2.3' < input.yaml > output.yaml
+
+  # Update files in-place
+  %[1]s -in values.yaml -out values.yaml '$.image.tag' '2.0'
+
+  # Update a comment
+  %[1]s --comment '$.service.port' 'Exposed port' < values.yaml
+`, os.Args[0])
+	}
 
 	flag.Parse()
 
 	if len(flag.Args()) != 2 {
-		slog.Error("Usage: yatch [-in <input file>] [-out <output file>] [--comment] <jsonpath> <value>")
+		flag.Usage()
 		os.Exit(1)
 	}
 
-	if err := exec(); err != nil {
-		slog.Error("Fatal", "err", err)
+	if err := exec(*in, *out, *comment, flag.Arg(0), flag.Arg(1)); err != nil {
+		slog.Error("Error", "err", err)
 		os.Exit(1)
 	}
 }
 
-func exec() error {
-	jsonpath, value := flag.Arg(0), flag.Arg(1)
-
-	reader, err := reader()
+func exec(in, out string, comment bool, jsonpath, value string) error {
+	reader, err := reader(in)
 	if err != nil {
-		return fmt.Errorf("error opening input, %w", err)
+		return fmt.Errorf("error opening input: %w", err)
 	}
-
-	writer, err := writer()
-	if err != nil {
-		return fmt.Errorf("error opening output, %w", err)
-	}
+	defer reader.Close()
 
 	yamlfile, err := yatch.NewFile(reader)
 	if err != nil {
-		return fmt.Errorf("error parsing input content, %w", err)
+		return fmt.Errorf("error parsing input content: %w", err)
 	}
 
-	if *comment {
+	if comment {
 		err = yamlfile.PatchComment(jsonpath, value)
 	} else {
 		err = yamlfile.Patch(jsonpath, value)
 	}
 
 	if err != nil {
-		return fmt.Errorf("error patching file, %w", err)
+		return fmt.Errorf("error while patching: %w", err)
 	}
 
-	_, err = writer.Write(yamlfile.Content())
+	writer, err := writer(out)
 	if err != nil {
-		return fmt.Errorf("error writing output, %w", err)
+		return fmt.Errorf("error opening output: %w", err)
+	}
+	defer writer.Close()
+
+	if _, err = writer.Write(yamlfile.Content()); err != nil {
+		return fmt.Errorf("error writing output: %w", err)
 	}
 
 	return nil
 }
 
-func reader() (reader io.ReadCloser, err error) {
-	if *in == "" {
+func reader(in string) (io.ReadCloser, error) {
+	if in == "" {
 		return os.Stdin, nil
 	}
-
-	return os.Open(*in)
+	return os.Open(in)
 }
 
-func writer() (writer io.WriteCloser, err error) {
-	if *out == "" {
+func writer(out string) (io.WriteCloser, error) {
+	if out == "" {
 		return os.Stdout, nil
 	}
-
-	return os.Create(*out)
+	return os.Create(out)
 }
